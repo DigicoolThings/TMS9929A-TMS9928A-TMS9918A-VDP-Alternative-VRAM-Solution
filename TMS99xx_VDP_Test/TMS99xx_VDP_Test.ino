ecd066a76a84e999de7a0d7722863676bd95a6ac
Greg <Greg@DigicoolThings.com>
2022-11-30 15:38:04 +1300

    Default the code to full byte wide VRAM use (i.e. low nibble only VRAM code is commented out by default) +  Add additional code commenting and additional color table setup options (supporting byte wide VRAM).

--- TMS99xx_VDP_Test/TMS99xx_VDP_Test.ino
+++ TMS99xx_VDP_Test/TMS99xx_VDP_Test.ino
@@ -10,7 +10,7 @@
 *
 * VDP IC is tested via stepping through the 14 different Backdrop (and Border) colors.
 * VRAM is utilised for displaying a single 8x8 Graphics 1 pattern in all 768 screen positions.
-* testVRAM() does a multi-pass test of the VRAM using either a nibble or byte pattern,  
+* testVRAM() does a multi-pass test of the VRAM using either a nibble or byte wide pattern,  
 *   allowing for testing with only nibble wide VRAM (eg. I was testing a circuit with a single 4464).  
 *
 * Open your Serial Monitor for progress and VRAM test result reporting. :)
@@ -43,7 +43,7 @@
 uint8_t readByteVDP(bool vramOrRegister) {
 /*
 * Reads a byte from VDP
-* vramOrRegister specifies accesss MODE 
+* vramOrRegister specifies access MODE 
 * VRAM access = LOW, Register access = HIGH 
 * nb. Can be called directly or see Convenience functions below
 */
@@ -192,9 +192,13 @@ static int vramTest() {
 /*
 * Multi-pass test of the VRAM using either a nibble or byte pattern
 * Select a testValues[] array with either byte or nibble only values
+* ie. If testing with a single 4464 DRAM chip located in the lower nibble.
 */
-//  uint8_t testValues[] = {0x00, 0xFF, 0x55, 0xAA};
-  uint8_t testValues[] = {0x00, 0x0F, 0x05, 0x0A};
+// Either, Byte wide memory testing values
+  uint8_t testValues[] = {0x00, 0xFF, 0x55, 0xAA};
+// Or, Low nibble only testing values (high nibble zeroed)
+//  uint8_t testValues[] = {0x00, 0x0F, 0x05, 0x0A};
+
   char hexString[9];
   bool testFail = false;
   uint8_t testByte;
@@ -209,7 +213,6 @@ static int vramTest() {
     Serial.println( "Testing Pattern: 0b" + String(hexString) );
     setVramWriteAddress(0x0000);
     for (uint16_t lp = 0; lp<16384; lp++) {
-   
       writeVramByteVDP(testByte);
     }
 
@@ -218,6 +221,7 @@ static int vramTest() {
       testAddr = lp;
       
       byteRead = readVramByteVDP();
+// In lower nibble only testing you might want to mask for lower nibble only
 //      byteRead = readVramByteVDP()  & 0x0F;
 
       if (byteRead != testByte) {
@@ -266,10 +270,11 @@ void vramSetup() {
   // Setup Test Pattern 0 in Pattern Table
   Serial.println("Setup Test Pattern 0 in Pattern Table");
   setVramWriteAddress(0x0800);
-  // Use for loop if you'd like to repeat the pattern
+  // Use following for loop if you'd like to repeat the pattern
 //  for (uint16_t lp = 0; lp<2048; lp=lp+8) {
 
   // Pattern using low nibble only (eg. for single 4464 only testing)  
+/*   
     writeVramByteVDP(0x00); // 00000000
     writeVramByteVDP(0x04); // 00000100
     writeVramByteVDP(0x0A); // 00001010
@@ -278,9 +283,9 @@ void vramSetup() {
     writeVramByteVDP(0x04); // 00000100
     writeVramByteVDP(0x0A); // 00001010
     writeVramByteVDP(0x00); // 00000000
+*/
 
   // Pattern using full byte pattern (for dual 4464 or other bytewide memory testing)
-/*   
     writeVramByteVDP(0x00); // 00000000
     writeVramByteVDP(0x18); // 00011000
     writeVramByteVDP(0x24); // 00100100
@@ -289,10 +294,10 @@ void vramSetup() {
     writeVramByteVDP(0x42); // 01000010
     writeVramByteVDP(0x42); // 01000010
     writeVramByteVDP(0x00); // 00000000 
-*/
+
 //   }
 
-  // Simple VRAM Test by reading and reporting Pattern 0 just stored in Pattern Table
+  // Optional simple VRAM Test by reading and reporting the Pattern 0 just stored in Pattern Table
 /*
   Serial.println("Reading back Test Pattern 0 from Pattern Table");
   setVramReadAddress(0x0800);
@@ -317,9 +322,31 @@ void vramSetup() {
 */
   Serial.println("Setup Color Table");
   setVramWriteAddress(0x2000);
-  writeVramByteVDP(0x01); // Tramsparent (color 0) on bits, Black (color 1) off bits 
+  // The following pattern color is the only one that will work with low nibble only memory (Transparent on bits)
+//  writeVramByteVDP(0x01); // Transparent (color 0) on bits, Black (color 1) off bits 
+
+  // The following pattern colors require byte wide memory (on bits a color other than Transparent)
+//  writeVramByteVDP(0x11); // Black (color 1) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0x21); // Medium Green (color 2) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0x31); // Light Green (color 3) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0x41); // Dark Blue (color 4) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0x51); // Light Blue (color 5) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0x61); // Dark Red (color 6) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0x71); // Cyan (color 7) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0x81); // Medium Red (color 8) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0x91); // Light Red (color 9) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0xA1); // Dark Yellow (color 10) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0xB1); // Light Yellow (color 11) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0xC1); // Dark Green (color 12) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0xD1); // Magenta (color 13) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0xE1); // Grey (color 14) on bits, Black (color 1) off bits 
+//  writeVramByteVDP(0xF1); // White (color 15) on bits, Black (color 1) off bits 
+
+  // This is my favorite combination: White for on bits and Dark Blue for off bits
+  writeVramByteVDP(0xF4); // White (color 15) on bits, Dark Blue (color 4) off bits 
+  
+  // Following example will instead setup the same Color for all 256 possible Patterns
 /*  
-  // Following will instead setup the same Color for all 256 possible Patterns
   for (uint16_t lp = 0; lp<32; lp++) {
     writeVramByteVDP(0x01);
   }
@@ -342,6 +369,7 @@ void setup() {
   digitalWrite(CSR, HIGH);  
   pinMode(RESET, OUTPUT);
   digitalWrite(RESET, HIGH);
+
   // Reset TMS VDP
   delayMicroseconds(5);
   digitalWrite(RESET, LOW);
@@ -419,7 +447,7 @@ void setup() {
 */
   writeRegisterByteVDP(0x01, 7); // Transparent Text / Black Backdrop
 
-  // Use the following vram call block if you only want single VRAM test (and setup)
+  // Use the following VRAM call code block if you only want single initial VRAM test (and setup)
 /**/  
   // Perform 16KB VRAM test
   vramTest();
@@ -443,8 +471,8 @@ uint16_t testCountSuccess = 0;
 uint16_t testCountFail = 0;
 
 void loop() {
-  // Use the following vram call etc block if you want repeated VRAM tests (and subsequent VRAM setup)
-/*
+  // Use the following vram call etc code block if you want repeated VRAM tests (and subsequent VRAM setup)
+/*  
   Serial.println();
   
   // Optional - Disable the Display Area during VRAM Test
@@ -462,7 +490,8 @@ void loop() {
 
   // Reenable the Display Area after VRAM Test (only necessary if we disabled it above)
   writeRegisterByteVDP(0xC0, 1); // Graphics I or II Mode, 8x8 Sprites, 16KB VRAM, Display Area Enabled
-*/
+*/  
+
   
   // Rotate through the 14 different Backdrop colors (2..15)
   // Excludes Transparent (0) and Black (1)
